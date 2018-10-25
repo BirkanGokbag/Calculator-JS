e5becb855417d93c8bfaba3b065637bdb22eca8b
Michael Radey <radeymichael@gmail.com>
2018-10-25 16:23:32 -0400

    update

--- js/calculator.js
+++ js/calculator.js
@@ -8,26 +8,28 @@ Contains the javascript for the calculator
   This method will update the calcultor's screen when a number button is pressesd
 */
 function numberClick(number){
-
+  
   display = document.getElementById("display").innerHTML;
   if(display.length < 17){
-    if(display.toString() == "0" && number != '.'){
+    if(display.toString() == "0" && number != '.' || this.clearScreen){
       document.getElementById("display").innerHTML = number;   
     }else if(number == '.' && display.indexOf(number) == -1){
       document.getElementById("display").innerHTML = display+ number;
     }else if(number != '.'){
       document.getElementById("display").innerHTML = display + number;
     }
+    this.clearScreen = false;
   }
 }
 
 //The function that handles the operations that require at least two numbers, such as +, -, /, *
 function operationClick(operation){
 
+  this.clearScreen = true;
+
   // Check if the user entered an operation before
   if(this.lastNumber==undefined && this.lastOperation == undefined){
     this.lastNumber = parseFloat(document.getElementById("display").innerHTML);
-    document.getElementById("display").innerHTML = 0;
   }else{
 
     // See what operation the user entered
@@ -56,8 +58,8 @@ function operationClick(operation){
 
 
 // Clears the screen to 0
-function clear(){
-  document.getElementById("display").innerHTML = 0;
+function clearDisplay(){
+  document.getElementById("display").innerHTML = "0"; 
 }
 
 // Takes the squareroot of the number on the screen
