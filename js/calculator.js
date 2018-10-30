c2b90f52f7f3c8671b07fe5cd4d8e77a767456f6
BirkanGokbag <birkan.gokbag@gmail.com>
2018-10-29 23:21:42 -0400

    Created the functionality for the buttons n!, log, and C.

--- js/calculator.js
+++ js/calculator.js
@@ -3,7 +3,6 @@ Contributors: Rajeev Ravi,
 Contains the javascript for the calculator
 */
 
-
 var calculatorBase = null;
 
 function createObjects(){
@@ -11,21 +10,32 @@ function createObjects(){
   mainArg: 0, //The main argument usually on the calculator's display.
   hiddenArg:0, //The argument that is hidden and used for the various operations.
   memoryArg:0, //The memory variable
+  empty_out:0, //Where some functions that reset the display are stored
   display: document.getElementById("display"), //A direct link to the display's html.
   clearScreen:false,
-  songList: ["pump_up.mp3","musical.mp3","SusumuHirasawa1.mp3","SusumuHirasawa2.mp3"] //Birkan's bad musical taste.
+  songList: ["pump_up.mp3","musical.mp3","SusumuHirasawa1.mp3","SusumuHirasawa2.mp3"] //Birkan's great musical taste.
 }
   //The number buttons must have access to all of the parameters in the one calculatorBase object.
   NumberButton.prototype = calculatorBase;
 
   //The operation buttons must have access to all of the parameters as well.
-OperationButton.prototype = calculatorBase;
-  
+  OperationButton.prototype = calculatorBase;
+
   //create the number buttons
   numberButtons = document.getElementsByClassName('number');
-  for(i = 0; i < numberButtons.length; i++){ 
-    NumberButton(numberButtons[i]); 
+  for(i = 0; i < numberButtons.length; i++){
+    NumberButton(numberButtons[i]);
   }
+
+  // Create the log button and assign it to the logarithmic function
+  document.getElementById("log").addEventListener("click", logarithmic, false);
+  // Create the clear button.
+  document.getElementById("clearButton").addEventListener("click", clearCalculator, false);
+  // Create the factorial button.
+  document.getElementById("factorial").addEventListener("click", factorial, false);
+
+
+
 
   //Now, all of the operation buttons need to be created.
 }
@@ -47,9 +57,15 @@ Constructor function for the NumberButton. NumberButtons are the 0-9 digits
 displayed on the calculator's face.
 */
 function NumberButton(htmlElement) {
-    
+
     htmlElement.onclick = function(){
-      
+
+      //If there was a log operation, start from 0, then reset the log condition
+      if (calculatorBase.empty_out == 1){
+        calculatorBase.empty_out = 0;
+        calculatorBase.display.innerHTML = 0;
+      }
+
       display = calculatorBase.display.innerHTML;
       number = htmlElement.innerHTML;
       if(display.length < calculatorBase.display.clientWidth/17 || calculatorBase.clearScreen){
@@ -61,7 +77,7 @@ function NumberButton(htmlElement) {
           calculatorBase.display.innerHTML = display + number;
         }
         calculatorBase.clearScreen = false;
-        calculatorBase.mainArgument = parseFloat(calculatorBase.display.innerHTML);
+        calculatorBase.mainArg = parseFloat(calculatorBase.display.innerHTML);
       }
 
     }
@@ -75,4 +91,46 @@ function OperationButton(htmlElement,onClickFunction){
   htmlElement.onclick = onClickFunction;
 }
 
+function logarithmic() {
+  valueInDisplay = parseFloat(document.getElementById("display").innerHTML);
+  //If the number is negative or 0, then cannot get the log of the value
+  if (valueInDisplay <= 0){
+    calculatorBase.display.innerHTML = "Not A Number."
+  }
+  else{
+    calculatorBase.display.innerHTML = Math.log(parseFloat(document.getElementById("display").innerHTML));
+  }
+  //Log operation had happened
+  calculatorBase.empty_out = 1;
+}
 
+function clearCalculator() {
+
+  //Clear the display, and all the arguments.
+  calculatorBase.display.innerHTML = 0;
+  calculatorBase.hiddenArg = 0;
+  calculatorBase.memoryArg = 0;
+  calculatorBase.mainArg = 0;
+  calculatorBase.empty_out = 0;
+}
+
+function factorial(){
+  displayValue = calculatorBase.display.innerHTML;
+  if (displayValue != 0){
+    tempVariable = displayValue;
+    result = 1;
+    if (tempVariable < 0){
+      tempVariable = tempVariable * -1;
+      result = -1;
+    }
+    while (tempVariable != 0){
+      result = result * tempVariable;
+      tempVariable--;
+    }
+    calculatorBase.display.innerHTML = result;
+  } else {
+    calculatorBase.display.innerHTML = 1;
+  }
+  //The next press will out the display if its a number
+  calculatorBase.empty_out = 1;
+}
